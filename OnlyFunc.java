6ffa7dd43c947dec41a9d9334278b1bcb4d13dbc
Dev? <ayanthakur.mumensom@outlook.com>
2026-08-18 17:23:03 +0200

    Create OnlyFunc.java

---
+++ OnlyFunc.java
@@ -0,0 +1,19 @@
+public static int findSecondLargest(int[] arr) {
+    if (arr.length < 2) {
+        return -1;
+    }
+
+    int largest = Integer.MIN_VALUE;
+    int secondLargest = Integer.MIN_VALUE;
+
+    for (int num : arr) {
+        if (num > largest) {
+            secondLargest = largest;
+            largest = num;
+        } else if (num > secondLargest && num != largest) {
+            secondLargest = num;
+        }
+    }
+
+    return secondLargest == Integer.MIN_VALUE ? -1 : secondLargest;
+}
